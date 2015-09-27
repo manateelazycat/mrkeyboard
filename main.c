67c89e5ba2703904e853ab42245164ab6e9e48d3
Andy Stewart <lazycat.manatee@gmail.com>
2015-09-27 11:40:59 +0800

    Add two features: move window and toggle window maximize.

--- main.c
+++ main.c
@@ -5,6 +5,41 @@
 #include <gtk/gtk.h>
 #include <stdlib.h>
 
+static gboolean is_left_button(ClutterEvent *event) {
+    return (clutter_event_type(event) == CLUTTER_BUTTON_PRESS && clutter_event_get_button(event) == CLUTTER_BUTTON_PRIMARY);
+}
+
+static void toggle_window(GtkWindow *window) {
+    GdkWindowState state = gdk_window_get_state(gtk_widget_get_window(GTK_WIDGET(window)));
+    if (state & GDK_WINDOW_STATE_MAXIMIZED) {
+        gtk_window_unmaximize(window);
+    } else {
+        gtk_window_maximize(window);
+    }
+}
+
+static void move_window(ClutterActor *area, ClutterEvent *event, GtkWindow *window) {
+    if (is_left_button(event)) {
+        if (clutter_event_get_click_count(event) == 2) {
+            toggle_window(window);
+        } else {
+            GdkWindow *gdk_window = gtk_widget_get_window(GTK_WIDGET(window));
+            gfloat x = 0;
+            gfloat y = 0;
+            gint window_x = 0;
+            gint window_y = 0;
+            clutter_event_get_coords(event, &x, &y);
+            gdk_window_get_origin(GDK_WINDOW(gdk_window), &window_x, &window_y);
+            gtk_window_begin_move_drag(window,
+                                       CLUTTER_BUTTON_PRIMARY,
+                                       window_x + x,
+                                       window_y + y,
+                                       clutter_event_get_time(event)
+                                       );
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     /* Init clutter */
     if (gtk_clutter_init(&argc, &argv) != CLUTTER_INIT_SUCCESS) {
@@ -34,15 +69,67 @@ int main(int argc, char *argv[]) {
     clutter_actor_show(stage);
     
     /* Init actor */
+    ClutterActor *top_box = mx_box_layout_new();
+    mx_box_layout_set_orientation(MX_BOX_LAYOUT(top_box), MX_ORIENTATION_HORIZONTAL);
+    clutter_actor_set_position(top_box, 0, 0);
+    clutter_actor_set_size(top_box, 800, 40);
+    clutter_actor_add_child(CLUTTER_ACTOR(stage), top_box);
+    clutter_actor_show(top_box);
+        
     ClutterColor name_color = {80, 80, 80, 255};
     ClutterActor *name_text = clutter_text_new_full("Mono 12", "Mr. keyboard", &name_color);
-    clutter_actor_set_position(name_text, 0, 0);
-    clutter_actor_add_child(CLUTTER_ACTOR(stage), name_text);
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(top_box), CLUTTER_ACTOR(name_text), -1);
+	clutter_container_child_set(CLUTTER_CONTAINER(top_box),
+                                name_text,
+                                "expand", FALSE,
+                                "x-fill", FALSE,
+                                "y-fill", FALSE,
+                                "y-align", MX_ALIGN_MIDDLE,
+                                "x-align", MX_ALIGN_START,
+                                NULL);    
+    clutter_actor_set_reactive(name_text, TRUE);
+    g_signal_connect(name_text, "button-press-event", G_CALLBACK(move_window), window);
     
     ClutterActor *search_entry = mx_entry_new();
-    clutter_actor_set_position(search_entry, 200, 0);
-    clutter_actor_add_child(CLUTTER_ACTOR(stage), search_entry);
+    mx_entry_set_hint_text(MX_ENTRY(search_entry), "Type you want");
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(top_box), CLUTTER_ACTOR(search_entry), -1);
+	clutter_container_child_set(CLUTTER_CONTAINER(top_box),
+                                search_entry,
+                                "expand", TRUE,
+                                "x-fill", TRUE,
+                                "y-fill", FALSE,
+                                "y-align", MX_ALIGN_MIDDLE,
+                                "x-align", MX_ALIGN_MIDDLE,
+                                NULL);    
     
+    ClutterColor status_color = {80, 80, 80, 255};
+    ClutterActor *status_text = clutter_text_new_full("Mono 12", "", &status_color);
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(top_box), CLUTTER_ACTOR(status_text), -1);
+    clutter_actor_set_size(status_text, 100, 40);
+    clutter_actor_set_reactive(status_text, TRUE);
+    g_signal_connect(status_text, "button-press-event", G_CALLBACK(move_window), window);
+    
+    ClutterActor *window_button_box = mx_box_layout_new();
+    mx_box_layout_set_orientation(MX_BOX_LAYOUT(window_button_box), MX_ORIENTATION_HORIZONTAL);
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(top_box), CLUTTER_ACTOR(window_button_box), -1);
+	clutter_container_child_set(CLUTTER_CONTAINER(top_box),
+                                window_button_box,
+                                "expand", FALSE,
+                                "x-fill", FALSE,
+                                "y-fill", FALSE,
+                                "y-align", MX_ALIGN_MIDDLE,
+                                "x-align", MX_ALIGN_END,
+                                NULL);    
+    
+    ClutterActor *min_button = mx_button_new_with_label("-");
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(window_button_box), CLUTTER_ACTOR(min_button), -1);
+
+    ClutterActor *max_button = mx_button_new_with_label("+");
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(window_button_box), CLUTTER_ACTOR(max_button), -1);
+
+    ClutterActor *close_button = mx_button_new_with_label("x");
+    mx_box_layout_add_actor(MX_BOX_LAYOUT(window_button_box), CLUTTER_ACTOR(close_button), -1);
+
     ClutterActor *window_texture = clutter_x11_texture_pixmap_new_with_window(0x540002b);
     clutter_x11_texture_pixmap_set_automatic(CLUTTER_X11_TEXTURE_PIXMAP(window_texture), TRUE);
     clutter_actor_set_position(window_texture, 0, 40);
