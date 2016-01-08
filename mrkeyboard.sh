39ba513781a685dee390deaaf810b20bcd3a42e6
Andy Stewart <lazycat.manatee@gmail.com>
2016-01-08 22:03:21 +0800

    Revert "Remove invalid event grep command."
    This reverts commit 12ea1a358a3646654131a86d4c3f81c1df24177c.

--- mrkeyboard.sh
+++ mrkeyboard.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export CLUTTER_BACKEND=x11
-./main $* 2>&1 | stdbuf -o0 grep -ivn -e '^(main:.*Missing *name *of *pseudo-class$' -e '^(main:.*Whoever *translated *default:LTR *did *so *wrongly.$' -e '^$' -e '^\*\* (main:[0-9].*): WARNING \*\*: Couldn.* connect to accessibility bus: Failed to connect to socket /tmp'
+./main $* 2>&1 | stdbuf -o0 grep -ivn -e '^(main:.*It *is *most *likely *synthesized *outside *Gdk/GTK+$'  -e '^(main:.*Missing *name *of *pseudo-class$' -e '^(main:.*Whoever *translated *default:LTR *did *so *wrongly.$' -e '^$' -e '^\*\* (main:[0-9].*): WARNING \*\*: Couldn.* connect to accessibility bus: Failed to connect to socket /tmp'
